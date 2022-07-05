006dd98d281a8e24339167cc4017dedf434ae7bf
maheshkumartippanu <maheshkumartippanu@gmail.com>
2022-07-05 22:32:42 +0530

    first commit

---
+++ src/main/java/com/example/firstspringapp/FirstspringappApplication.java
@@ -0,0 +1,14 @@
+package com.example.firstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstspringappApplication.class, args);
+     //   System.out.println("Hello I am From BridgeLabz");
+    }
+
+}
